1073c18b7f0cea9d2160ac21886ceeb26cced119
henry-e-n <henry.nachman@utexas.edu>
2025-07-22 15:46:17 -0500

    package data fix?

--- setup.py
+++ setup.py
@@ -7,7 +7,7 @@ from subprocess import check_call
 
 HERE         = pathlib.Path(__file__).parent
 PACKAGE_NAME = 'thermal_model'
-VERSION      = '0.0.7.2'
+VERSION      = '0.0.7.3'
 AUTHOR       = 'Henry Nachman'
 AUTHOR_EMAIL = 'hen367@my.utexas.edu'
 URL          = 'https://github.com/henry-e-n/Balloon_Modeling_Tools'
@@ -88,4 +88,5 @@ setup(
 	packages=find_packages(),
 	package_data={
 		'thermal_model': ['*.json', '*.txt', '*.csv', '*.png', '*.css', '*.html'],
-		})	
+		},
+	include_package_data=True)	
